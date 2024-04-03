ac61b4a5ccb80cee692fca7b44b3466197ab27c4
Don Fox <don@thedataincubator.com>
2024-04-03 18:44:05 -0400

    Turn off telemetry

--- src/sql/telemetry.py
+++ src/sql/telemetry.py
@@ -1,3 +1,4 @@
+from ploomber_core.telemetry import telemetry
 from ploomber_core.telemetry.telemetry import Telemetry
 
 try:
@@ -6,6 +7,11 @@ except ModuleNotFoundError:
     from importlib_metadata import version
 
 
+def check_telemetry_enabled():
+    return False
+
+
+telemetry.check_telemetry_enabled = check_telemetry_enabled
 telemetry = Telemetry(
     api_key="phc_P9SpSeypyPwxrMdFn2edOOEooQioF2axppyEeDwtMSP",
     package_name="jupysql",
